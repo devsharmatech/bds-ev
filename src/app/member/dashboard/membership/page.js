db04aa5b268127474be5f7ea162e93491d1d4501
devsharmatech <devp7536@gmail.com>
2026-01-22 19:06:47 +0530

    update 22-12

--- src/app/member/dashboard/membership/page.js
+++ src/app/member/dashboard/membership/page.js
@@ -1879,7 +1879,7 @@ export default function MembershipCardPage() {
                       </div>
                     )}
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 hidden">
                       {/* ID Card Upload */}
                       <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">
